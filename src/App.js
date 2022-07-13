0c225129cbaaf80d2066a498b9732295cee70f91
dartan11 <darrentantsukai@gmail.com>
2022-07-13 16:17:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import Clock from "./components/Clock.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class App extends React.Component {
   render() {
@@ -8,9 +13,14 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <br></br>
+          <Container fluid>
+          <Row>
+          <Col><Clock timeZone="Asia/Singapore" /></Col>
+          <Col><Clock timeZone="Europe/London" /></Col>
+          <Col><Clock timeZone="America/Los_Angeles" /></Col>
+          </Row>
+          </Container>
         </header>
       </div>
     );
---
+++ src/components/Clock.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+componentDidMount(){
+  this.timerID = setInterval(()=>this.setState({date:new Date(),}),1000);
+}
+
+componentWillUnmount(){
+  clearInterval(this.timerID)
+}
+
+  render(){
+    return<p>{this.props.timeZone}:{this.state.date.toLocaleString("en-GB", {timeZone: this.props.timeZone})}</p>}
+}
